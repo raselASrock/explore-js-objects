43d0b809c52b2ea82ce8598695cc6806b1eea6b6
raselASrock <mdraselm413@gmail.com>
2022-08-31 01:05:04 +0600

    type-object

--- object.js
+++ object.js
@@ -1,2 +1,53 @@
-// Create Object using object Literals
+// 1. Create Object using object Literals
 const player = {};
+player.name = 'Small Nirob';
+player.speciality = 'batsman';
+player.bat = function(){
+    // console.log('Swing your bat')
+}
+player.bat ()
+// console.log(player);
+
+const student = {
+    name: 'Pandey',
+    job: 'Khay Andey',
+    ball: function(){
+        console.log('Throw the ball')
+    },
+    salary: 10000,
+}
+
+// 2. Object Constructor
+const person = new Object()
+// console.log(person)
+
+//  3. Object create method
+// const item = Object.create(null);
+const atel = Object.create(student)
+// console.log(item);
+// console.log(atel);
+// console.log(atel.name);
+// console.log(atel.job);
+
+//  4. Class
+
+class Person{
+    name = 'abul';
+    address = 'sodor-ghat'
+    constructor(age){
+        this.age = age;
+    }
+}
+
+const person1 = new Person(56)
+console.log(person1);
+
+// Function
+function Car (model, price){
+    this.model = model;
+    this.price = price;
+}
+
+const tesla = new Car('325k756po', '$32560000')
+
+console.log(tesla);
