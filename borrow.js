4486645e589c833be27b50508e5354b1dd526a1a
raselASrock <mdraselm413@gmail.com>
2022-09-01 00:02:32 +0600

    optional

---
+++ borrow.js
@@ -0,0 +1,31 @@
+const kodomAli = {
+    name: 'Kodom Ali',
+    money: 5000,
+    study: 'Math',
+    subjects: ['calculus', 'aljebra', 'geometry', ],
+    exam: function(){
+        // console.log(this.name, 'is participating in an exam');
+        return this.name + ' is participating in an exam'
+    },
+    improveExam: function(subject){
+        this.exam();
+        return `${this.name} is taking improvement exam on ${subject}`
+    },
+    treatDe: function(amount, tips){
+        this.money = this.money - amount - tips;
+        return this.money;
+    }
+}
+
+const result = kodomAli.exam();
+const badamAli = {
+    name: 'kacha badam',
+    money: 8000,
+
+}
+
+const result2 = kodomAli.exam.call(badamAli)
+console.log(result2);
+
+const badamMoney = kodomAli.treatDe.call(badamAli, 400, 50)
+console.log(badamMoney);
