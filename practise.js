107f33502598f3467c13a977e8948e81cfdd433f
raselASrock <mdraselm413@gmail.com>
2022-09-01 18:48:55 +0600

    object

---
+++ practise.js
@@ -0,0 +1,19 @@
+
+// Practise
+/* 
+1. arrow function
+2. template string
+3. forEach, filter, find, map
+4. API: fetch
+5. DOM set Element
+*/
+
+// Task
+/* 
+1. object with 6 propertise: ( number, string, boolean, object, function(method, access any object property and return it), arrey)
+2. template string 
+3. arrow function 
+    3.1 no params return 89
+    3.2 number as param and return it by dividing by 17
+    3.3 two param: add 7 to the first number and add 5 to the second number
+*/
